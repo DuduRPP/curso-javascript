1b6fccc882dcbb5e04d81ccc567b2147a95784cb
DuduRPP <dudurpp1@gmail.com>
2022-04-18 23:10:19 -0300

    Logica basica do jogo implementada

--- DOM/GuessMyNumber/script.js
+++ DOM/GuessMyNumber/script.js
@@ -1,9 +1,27 @@
 "use strict";
 
+const secretNumber = Math.trunc(Math.random() * 100) + 1;
+let score = 100;
+document.querySelector(".number").textContent = secretNumber;
+
 document.querySelector(".check").addEventListener("click", () => {
   const guess = Number(document.querySelector(".guess").value);
 
-  if (!guess) {
-    document.querySelector(".message").textContent = "🚫 No number!";
+  if (score > 0) {
+    if (!guess) {
+      document.querySelector(".message").textContent = "🚫 No Number!";
+    } else if (guess == secretNumber) {
+      document.querySelector(".message").textContent = "🎉 Correct Number!";
+    } else if (guess > secretNumber) {
+      document.querySelector(".message").textContent = "⏬ Too High!";
+      score--;
+    } else if (guess < secretNumber) {
+      document.querySelector(".message").textContent = "⏫ Too Low!";
+      score--;
+    }
+  } else {
+    document.querySelector(".message").textContent = "💥 You Lost The Game!";
   }
+
+  document.querySelector(".score").textContent = score;
 });
