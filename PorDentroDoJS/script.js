3705f72fe11a441351552e5c3832f217d6e81a4f
DuduRPP <dudurpp1@gmail.com>
2022-04-25 23:11:43 -0300

    Mais topicos adicionados

--- PorDentroDoJS/script.js
+++ PorDentroDoJS/script.js
@@ -5,3 +5,19 @@
 // 1.JS Runtime (JIT, Linguagens Compiladas/Interpretadas)
 
 // 2.Scopes & Call Stack
+
+// 3. Hoisting & TDZ
+
+// 4. Keyword this e diferenca entre funcoes normais e arrow functions
+
+const jonas = {
+  firstName: "Jonas",
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2022 - this.year);
+  },
+  greet: () => {
+    console.log(`Hey ${this.firstName}`);
+  },
+};
